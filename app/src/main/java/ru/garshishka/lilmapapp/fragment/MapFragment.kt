b98094ff53992c6233e65e6a6c271234244cd688
garshishka <garshiv@gmail.com>
2023-12-06 00:37:10 +0300

    Added buttons win no functionality yet

--- app/src/main/java/ru/garshishka/lilmapapp/fragment/MapFragment.kt
+++ app/src/main/java/ru/garshishka/lilmapapp/fragment/MapFragment.kt
@@ -1,6 +1,8 @@
 package ru.garshishka.lilmapapp.fragment
 
+import android.Manifest
 import android.content.Context.MODE_PRIVATE
+import android.content.pm.PackageManager
 import android.graphics.Rect
 import android.location.GpsStatus
 import android.os.Bundle
@@ -8,23 +10,39 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.events.MapListener
 import org.osmdroid.events.ScrollEvent
 import org.osmdroid.events.ZoomEvent
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import ru.garshishka.lilmapapp.R
 import ru.garshishka.lilmapapp.databinding.FragmentMapBinding
 
-class MapFragment: Fragment(), MapListener, GpsStatus.Listener {
+class MapFragment : Fragment(), MapListener, GpsStatus.Listener {
+    //Permission part
+    private var locationPermission = false
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                locationPermission = true
+            } else {
+                showPermissionSnackbar()
+            }
+        }
+
     private val binding: FragmentMapBinding by viewBinding(createMethod = CreateMethod.INFLATE)
+    private var focusOn = false
 
     lateinit var mMap: MapView
     lateinit var controller: IMapController
@@ -32,6 +50,7 @@ class MapFragment: Fragment(), MapListener, GpsStatus.Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkMapPermission()
     }
 
     override fun onCreateView(
@@ -53,46 +72,73 @@ class MapFragment: Fragment(), MapListener, GpsStatus.Listener {
         controller = mMap.controller
 
         mMyLocationOverlay.enableMyLocation()
-        mMyLocationOverlay.enableFollowLocation()
+        //mMyLocationOverlay.enableFollowLocation()
         mMyLocationOverlay.isDrawAccuracyEnabled = true
-//        mMyLocationOverlay.runOnFirstFix {
-//            runOnUiThread {
-//                controller.setCenter(mMyLocationOverlay.myLocation);
-//                controller.animateTo(mMyLocationOverlay.myLocation)
-//            }
-//        }
 
-        // val mapPoint = GeoPoint(latitude, longitude)
 
-        controller.setZoom(6.0)
+        controller.setZoom(17.5)
 
-        Log.e("TAG", "onCreate:in ${controller.zoomIn()}")
-        Log.e("TAG", "onCreate: out  ${controller.zoomOut()}")
+        Log.i("TAG", "onCreate:in ${controller.zoomIn()}")
+        Log.i("TAG", "onCreate: out  ${controller.zoomOut()}")
 
-        // controller.animateTo(mapPoint)
+        val mapPoint = GeoPoint(55.748225, 37.625184)
+        controller.animateTo(mapPoint)
+
         mMap.overlays.add(mMyLocationOverlay)
 
         mMap.addMapListener(this)
+
+        binding.apply {
+            focusButton.setOnClickListener {
+                it.setBackgroundColor(if(focusOn) requireContext().getColor(R.color.focus_on) else requireContext().getColor(R.color.focus_off))
+                focusOn = !focusOn
+            }
+        }
 
         return binding.root
     }
 
     override fun onScroll(event: ScrollEvent?): Boolean {
-        // event?.source?.getMapCenter()
-        Log.e("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
-        Log.e("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
-        //  Log.e("TAG", "onScroll   x: ${event?.x}  y: ${event?.y}", )
+        Log.i("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
+        Log.i("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
         return true
     }
 
     override fun onZoom(event: ZoomEvent?): Boolean {
-        //  event?.zoomLevel?.let { controller.setZoom(it) }
 
-        Log.e("TAG", "onZoom zoom level: ${event?.zoomLevel}   source:  ${event?.source}")
+        Log.i("TAG", "onZoom zoom level: ${event?.zoomLevel}   source:  ${event?.source}")
         return false
     }
 
     override fun onGpsStatusChanged(event: Int) {
         TODO("Not yet implemented")
+    }
+
+    private fun checkMapPermission() {
+        when {
+            context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } == PackageManager.PERMISSION_GRANTED -> {
+                locationPermission = true
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                showPermissionSnackbar()
+            }
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+
+    private fun showPermissionSnackbar() {
+        Snackbar.make(binding.osmmap, getString(R.string.need_geolocation), Snackbar.LENGTH_LONG)
+            .setAction(getString(R.string.permission)) {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            .show()
     }
 }
